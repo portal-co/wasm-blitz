b14e9574a0a41b0253664d63a2f0fadba2f4a5df
Graham Kelly <gkgoat6700@gmail.com>
2025-10-02 14:16:35 +0000

    js init

--- crates/blitz-js/src/lib.rs
+++ crates/blitz-js/src/lib.rs
@@ -1,6 +1,37 @@
 #![no_std]
 use core::{
     error::Error,
-    fmt::{Formatter, Write},
+    fmt::{Display, Formatter, Write},
 };
+
+use portal_solutions_blitz_common::{MachOperator, wasmparser::ValType};
 extern crate alloc;
+pub fn push(w: &mut impl Write, a: &(dyn Display + '_)) -> core::fmt::Result {
+    write!(w, "(stack=[...stack,{a}])")
+}
+pub fn pop(w: &mut impl Write) -> core::fmt::Result {
+    write!(w, "(([...stack,tmp]=stack),tmp)")
+}
+pub trait JsWrite: Write {
+    fn on_mach(&mut self, m: &MachOperator<'_>) -> core::fmt::Result {
+        match m {
+            MachOperator::StartFn { id, data } => write!(self, "function ${id}(...locals){{let stack=[],tmp;"),
+            MachOperator::Local(a, b) => write!(
+                self,
+                "locals=[...locals,{}];",
+                match b {
+                    ValType::F32 | ValType::F64 => "0",
+                    _ => "0n",
+                }
+            ),
+            MachOperator::StartBody => Ok(()),
+
+            MachOperator::Operator(o) => match o{
+                _ => todo!()
+            },
+            MachOperator::EndBody => write!(self, "}}"),
+            _ => todo!(),
+        }
+    }
+}
+impl<T: Write + ?Sized> JsWrite for T {}
