5496e2c0b34bbeb2942d571c3516776e37616504
Graham Kelly <gkgoat6700@gmail.com>
2025-10-08 05:47:55 +0000

    reewncode seperation

--- crates/blitz-reencode/src/lib.rs
+++ crates/blitz-reencode/src/lib.rs
@@ -20,60 +20,67 @@ impl MachTracker {
         }
     }
 }
+pub fn do_mach_instruction<E>(
+    r: &mut (impl Reencode<Error = E> + ?Sized),
+    a: &MachOperator<'_>,
+    state: &mut MachTracker,
+) -> Result<(), wasm_encoder::reencode::Error<E>> {
+    match a {
+        MachOperator::StartFn { id, data } => {}
+        MachOperator::Local { count: a, ty: b } => {
+            state.locals.push((*a, r.val_type(b.clone())?));
+        }
+        MachOperator::StartBody => {
+            state
+                .funcs
+                .push(wasm_encoder::Function::new(state.locals.drain(..)));
+        }
+        MachOperator::EndBody => {}
+        MachOperator::Operator(o) => {
+            let mut f = state.funcs.last_mut().unwrap();
+            match o {
+                Operator::Else => {
+                    if let Some(a) = state.dce_stack.last_mut() {
+                        *a = false
+                    }
+                }
+                Operator::If { .. } | Operator::Block { .. } | Operator::Loop { .. } => {
+                    state.dce_stack.push(false);
+                }
+                Operator::End => {
+                    state.dce_stack.pop();
+                }
+                Operator::Br { .. }
+                | Operator::BrTable { .. }
+                | Operator::Return
+                | Operator::ReturnCall { .. }
+                | Operator::ReturnCallIndirect { .. }
+                | Operator::ReturnCallRef { .. }
+                | Operator::Unreachable => {
+                    if let Some(a) = state.dce_stack.last_mut() {
+                        *a = true
+                    }
+                }
+                o => {
+                    if state.dce_stack.iter().any(|a| *a) {
+                        return Ok(());
+                    } else {
+                    }
+                }
+            };
+            f.instruction(&r.instruction(o.clone())?);
+        }
+        _ => todo!(),
+    };
+    Ok(())
+}
 pub trait ReencodeExt: Reencode {
     fn mach_instruction(
         &mut self,
         a: &MachOperator<'_>,
         state: &mut MachTracker,
     ) -> Result<(), wasm_encoder::reencode::Error<Self::Error>> {
-        match a {
-            MachOperator::StartFn { id, data } => {}
-            MachOperator::Local { count: a, ty: b } => {
-                state.locals.push((*a, self.val_type(b.clone())?));
-            }
-            MachOperator::StartBody => {
-                state
-                    .funcs
-                    .push(wasm_encoder::Function::new(state.locals.drain(..)));
-            }
-            MachOperator::EndBody => {}
-            MachOperator::Operator(o) => {
-                let mut f = state.funcs.last_mut().unwrap();
-                match o {
-                    Operator::Else => {
-                        if let Some(a) = state.dce_stack.last_mut() {
-                            *a = false
-                        }
-                    }
-                    Operator::If { .. } | Operator::Block { .. } | Operator::Loop { .. } => {
-                        state.dce_stack.push(false);
-                    }
-                    Operator::End => {
-                        state.dce_stack.pop();
-                    }
-                    Operator::Br { .. }
-                    | Operator::BrTable { .. }
-                    | Operator::Return
-                    | Operator::ReturnCall { .. }
-                    | Operator::ReturnCallIndirect { .. }
-                    | Operator::ReturnCallRef { .. }
-                    | Operator::Unreachable => {
-                        if let Some(a) = state.dce_stack.last_mut() {
-                            *a = true
-                        }
-                    }
-                    o => {
-                        if state.dce_stack.iter().any(|a| *a) {
-                            return Ok(());
-                        } else {
-                        }
-                    }
-                };
-                f.instruction(&self.instruction(o.clone())?);
-            }
-            _ => todo!(),
-        };
-        Ok(())
+        do_mach_instruction(self, a, state)
     }
 }
 impl<T: Reencode + ?Sized> ReencodeExt for T {}
