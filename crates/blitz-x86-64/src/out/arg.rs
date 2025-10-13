2ce00e5ed5e61697427da3e4884c4d5b851dfa8a
Graham Kelly <gkgoat6700@gmail.com>
2025-10-13 08:37:49 -0400

    add sized implmenntation

--- crates/blitz-x86-64/src/out/arg.rs
+++ crates/blitz-x86-64/src/out/arg.rs
@@ -4,15 +4,16 @@ use super::*;
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 #[non_exhaustive]
 pub enum ArgKind {
-    Reg { reg: Reg },
+    Reg { reg: Reg, size: MemorySize },
     Lit(u64),
 }
 impl ArgKind {
     pub fn display(&self, opts: X64Arch) -> ArgKindDisplay {
         match self {
-            ArgKind::Reg { reg } => {
-                ArgKindDisplay::Reg(X64Reg::display(reg, RegFormatOpts::default_with_arch(opts)))
-            }
+            ArgKind::Reg { reg, size } => ArgKindDisplay::Reg(X64Reg::display(
+                reg,
+                RegFormatOpts::default_with_arch_and_size(opts, *size),
+            )),
             ArgKind::Lit(i) => ArgKindDisplay::Lit(*i),
         }
     }
@@ -47,7 +48,10 @@ pub trait Arg {
 }
 impl Arg for Reg {
     fn kind(&self) -> ArgKind {
-        ArgKind::Reg { reg: self.clone() }
+        ArgKind::Reg {
+            reg: self.clone(),
+            size: Default::default(),
+        }
     }
     fn display(&self, opts: X64Arch) -> ArgKindDisplay {
         ArgKindDisplay::Reg(X64Reg::display(
@@ -57,6 +61,27 @@ impl Arg for Reg {
     }
     fn format(&self, f: &mut Formatter<'_>, opts: X64Arch) -> core::fmt::Result {
         X64Reg::format(self, f, &RegFormatOpts::default_with_arch(opts))
+    }
+}
+impl Arg for (Reg, MemorySize) {
+    fn kind(&self) -> ArgKind {
+        ArgKind::Reg {
+            reg: self.0.clone(),
+            size: self.1.clone(),
+        }
+    }
+    fn display(&self, opts: X64Arch) -> ArgKindDisplay {
+        ArgKindDisplay::Reg(X64Reg::display(
+            &self.0,
+            RegFormatOpts::default_with_arch_and_size(opts, self.1),
+        ))
+    }
+    fn format(&self, f: &mut Formatter<'_>, opts: X64Arch) -> core::fmt::Result {
+        X64Reg::format(
+            &self.0,
+            f,
+            &RegFormatOpts::default_with_arch_and_size(opts, self.1),
+        )
     }
 }
 impl Arg for ArgKind {
@@ -72,6 +97,6 @@ impl Arg for u64 {
         ArgKindDisplay::Lit(*self)
     }
     fn format(&self, f: &mut Formatter<'_>, opts: X64Arch) -> core::fmt::Result {
-        write!(f,"{self}")
+        write!(f, "{self}")
     }
 }
