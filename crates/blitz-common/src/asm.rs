0a367ada138fa3e86630646590ce37d00fb98f48
Graham Kelly <gkgoat6700@gmail.com>
2025-10-13 10:43:51 -0400

    add helpers

--- crates/blitz-common/src/asm.rs
+++ crates/blitz-common/src/asm.rs
@@ -3,5 +3,14 @@ use crate::*;
 pub struct Reg(pub u8);
 impl Reg {
     pub const CTX: Reg = Reg(255);
-  
+    pub const fn r32_swap_0_and_31(&self) -> Self {
+        match self.0 % 32 {
+            0 => Self(31),
+            31 => Self(0),
+            v => Self(v),
+        }
+    }
+    pub const fn r32(&self) -> Self{
+        return Self(self.0 % 32);
+    }
 }
