961f67c8a17ed5b1eb36a893bf3f20927de7924e
Graham Kelly <gkgoat6700@gmail.com>
2025-10-22 05:00:03 -0400

    opt dependency

--- crates/blitz-common/src/asm.rs
+++ crates/blitz-common/src/asm.rs
@@ -1,16 +1,3 @@
 use crate::*;
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
-pub struct Reg(pub u8);
-impl Reg {
-    pub const CTX: Reg = Reg(255);
-    pub const fn r32_swap_0_and_31(&self) -> Self {
-        match self.0 % 32 {
-            0 => Self(31),
-            31 => Self(0),
-            v => Self(v),
-        }
-    }
-    pub const fn r32(&self) -> Self {
-        return Self(self.0 % 32);
-    }
-}
+pub use portal_pc_asm_common::types as common;
+pub use portal_pc_asm_common::types::reg::Reg;
