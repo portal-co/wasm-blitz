c60886011efab37d988ed1c811f7b6a0dd771ea6
Graham Kelly <gkgoat6700@gmail.com>
2025-10-27 20:40:39 -0400

    instruction support

--- crates/blitz-common/src/dce.rs
+++ crates/blitz-common/src/dce.rs
@@ -1,3 +1,5 @@
+use wasm_encoder::Instruction;
+
 use crate::*;
 #[derive(Default)]
 pub struct DceStack(Vec<bool>);
@@ -26,6 +28,39 @@ pub fn dce(DceStack(stack): &mut DceStack, o: &Operator<'_>) -> bool {
         | Operator::ReturnCallIndirect { .. }
         | Operator::ReturnCallRef { .. }
         | Operator::Unreachable => {
+            if let Some(a) = stack.last_mut() {
+                *a = true
+            }
+        }
+        o => {
+            if stack.iter().any(|a| *a) {
+                return true;
+            } else {
+            }
+        }
+    };
+    return false;
+}
+pub fn dce_instr(DceStack(stack): &mut DceStack, o: &Instruction<'_>) -> bool {
+    match o {
+        Instruction::Else => {
+            if let Some(a) = stack.last_mut() {
+                *a = false
+            }
+        }
+        Instruction::If { .. } | Instruction::Block { .. } | Instruction::Loop { .. } => {
+            stack.push(false);
+        }
+        Instruction::End => {
+            stack.pop();
+        }
+        Instruction::Br { .. }
+        | Instruction::BrTable { .. }
+        | Instruction::Return
+        | Instruction::ReturnCall { .. }
+        | Instruction::ReturnCallIndirect { .. }
+        | Instruction::ReturnCallRef { .. }
+        | Instruction::Unreachable => {
             if let Some(a) = stack.last_mut() {
                 *a = true
             }
--- crates/blitz-common/src/passes.rs
+++ crates/blitz-common/src/passes.rs
@@ -11,6 +11,10 @@ macro_rules! dce_pass {
                         op: $crate::__::core::option::Option::Some(op),
                         annot,
                     } if $crate::dce::dce(dce_stack, op) => $crate::__::core::option::Option::None,
+                    $crate::MachOperator::Instruction {
+                        op,
+                        annot,
+                    } if $crate::dce::dce_instr(dce_stack, op) => $crate::__::core::option::Option::None,
                     o => {
                         if let $crate::MachOperator::EndBody = &o {
                             *dce_stack = $crate::dce::DceStack::new();
@@ -25,226 +29,226 @@ macro_rules! dce_pass {
         }
     };
 }
-#[macro_export]
-macro_rules! load_coalescing {
-    ($a:expr) => {
-        match match $a {
-            a => $crate::ops::IteratorExt::scan_mach(a, $crate::passes::load_coalescing_pass, ()),
-        } {
-            a => $crate::__::core::iter::Iterator::flatten(a),
-        }
-    };
-}
-pub fn load_coalescing_pass<'a, Annot: Clone>(
-    d: &mut FnData,
-    l: u32,
-    o: MachOperator<'a, Annot>,
-    x: &mut (),
-) -> Vec<MachOperator<'a, Annot>> {
-    match o {
-        MachOperator::StartBody => [
-            MachOperator::Local {
-                count: 2,
-                ty: ValType::I64,
-            },
-            MachOperator::StartBody,
-        ]
-        .into_iter()
-        .collect::<Vec<MachOperator<'a, Annot>>>(),
-        MachOperator::Operator { op: Some(o), annot } => match o {
-            Operator::I64Load8U { memarg } => [
-                Operator::I64Load { memarg },
-                Operator::I64Const { value: 0xff },
-                Operator::I64Add,
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I64Load16U { memarg } => [
-                Operator::I64Load { memarg },
-                Operator::I64Const { value: 0xffff },
-                Operator::I64Add,
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I64Load32U { memarg } => [
-                Operator::I64Load { memarg },
-                Operator::I64Const { value: 0xffff_ffff },
-                Operator::I64Add,
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Load8U { memarg } => [
-                Operator::I64Load { memarg },
-                Operator::I32WrapI64,
-                Operator::I32Const { value: 0xff },
-                Operator::I32Add,
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Load16U { memarg } => [
-                Operator::I64Load { memarg },
-                Operator::I32WrapI64,
-                Operator::I32Const { value: 0xffff },
-                Operator::I32Add,
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Load { memarg } => [Operator::I64Load { memarg }, Operator::I32WrapI64]
-                .into_iter()
-                .map(|v| MachOperator::Operator {
-                    op: Some(v),
-                    annot: annot.clone(),
-                })
-                .collect(),
-            Operator::I64Store8 { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::I64Const { value: !0xff },
-                Operator::I64And,
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I64Store16 { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::I64Const { value: !0xffff },
-                Operator::I64And,
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I64Store32 { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::I64Const {
-                    value: !0xffff_ffff,
-                },
-                Operator::I64And,
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Store8 { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::I64ExtendI32U,
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::I64Const { value: !0xff },
-                Operator::I64And,
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Store16 { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::I64ExtendI32U,
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::I64Const { value: !0xffff },
-                Operator::I64And,
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            Operator::I32Store { memarg } => [
-                Operator::LocalSet { local_index: l },
-                Operator::I64ExtendI32U,
-                Operator::LocalTee { local_index: l + 1 },
-                Operator::LocalGet { local_index: l + 1 },
-                Operator::I64Load {
-                    memarg: memarg.clone(),
-                },
-                Operator::LocalGet { local_index: l },
-                Operator::I64Or,
-                Operator::I64Store { memarg },
-            ]
-            .into_iter()
-            .map(|v| MachOperator::Operator {
-                op: Some(v),
-                annot: annot.clone(),
-            })
-            .collect(),
-            o => [MachOperator::Operator {
-                op: Some(o),
-                annot: annot.clone(),
-            }]
-            .into_iter()
-            .collect::<Vec<_>>(),
-        },
-        o => [o].into_iter().collect::<Vec<_>>(),
-    }
-}
-pub fn load_coalescing<'a, Annot: Clone>(
-    a: impl Iterator<Item = MachOperator<'a, Annot>>,
-) -> impl Iterator<Item = MachOperator<'a, Annot>> {
-    return a.scan_mach(load_coalescing_pass, ()).flatten();
-}
+// #[macro_export]
+// macro_rules! load_coalescing {
+//     ($a:expr) => {
+//         match match $a {
+//             a => $crate::ops::IteratorExt::scan_mach(a, $crate::passes::load_coalescing_pass, ()),
+//         } {
+//             a => $crate::__::core::iter::Iterator::flatten(a),
+//         }
+//     };
+// }
+// pub fn load_coalescing_pass<'a, Annot: Clone>(
+//     d: &mut FnData,
+//     l: u32,
+//     o: MachOperator<'a, Annot>,
+//     x: &mut (),
+// ) -> Vec<MachOperator<'a, Annot>> {
+//     match o {
+//         MachOperator::StartBody => [
+//             MachOperator::Local {
+//                 count: 2,
+//                 ty: ValType::I64,
+//             },
+//             MachOperator::StartBody,
+//         ]
+//         .into_iter()
+//         .collect::<Vec<MachOperator<'a, Annot>>>(),
+//         MachOperator::Operator { op: Some(o), annot } => match o {
+//             Operator::I64Load8U { memarg } => [
+//                 Operator::I64Load { memarg },
+//                 Operator::I64Const { value: 0xff },
+//                 Operator::I64Add,
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I64Load16U { memarg } => [
+//                 Operator::I64Load { memarg },
+//                 Operator::I64Const { value: 0xffff },
+//                 Operator::I64Add,
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I64Load32U { memarg } => [
+//                 Operator::I64Load { memarg },
+//                 Operator::I64Const { value: 0xffff_ffff },
+//                 Operator::I64Add,
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Load8U { memarg } => [
+//                 Operator::I64Load { memarg },
+//                 Operator::I32WrapI64,
+//                 Operator::I32Const { value: 0xff },
+//                 Operator::I32Add,
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Load16U { memarg } => [
+//                 Operator::I64Load { memarg },
+//                 Operator::I32WrapI64,
+//                 Operator::I32Const { value: 0xffff },
+//                 Operator::I32Add,
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Load { memarg } => [Operator::I64Load { memarg }, Operator::I32WrapI64]
+//                 .into_iter()
+//                 .map(|v| MachOperator::Operator {
+//                     op: Some(v),
+//                     annot: annot.clone(),
+//                 })
+//                 .collect(),
+//             Operator::I64Store8 { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::I64Const { value: !0xff },
+//                 Operator::I64And,
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I64Store16 { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::I64Const { value: !0xffff },
+//                 Operator::I64And,
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I64Store32 { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::I64Const {
+//                     value: !0xffff_ffff,
+//                 },
+//                 Operator::I64And,
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Store8 { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::I64ExtendI32U,
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::I64Const { value: !0xff },
+//                 Operator::I64And,
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Store16 { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::I64ExtendI32U,
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::I64Const { value: !0xffff },
+//                 Operator::I64And,
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             Operator::I32Store { memarg } => [
+//                 Operator::LocalSet { local_index: l },
+//                 Operator::I64ExtendI32U,
+//                 Operator::LocalTee { local_index: l + 1 },
+//                 Operator::LocalGet { local_index: l + 1 },
+//                 Operator::I64Load {
+//                     memarg: memarg.clone(),
+//                 },
+//                 Operator::LocalGet { local_index: l },
+//                 Operator::I64Or,
+//                 Operator::I64Store { memarg },
+//             ]
+//             .into_iter()
+//             .map(|v| MachOperator::Operator {
+//                 op: Some(v),
+//                 annot: annot.clone(),
+//             })
+//             .collect(),
+//             o => [MachOperator::Operator {
+//                 op: Some(o),
+//                 annot: annot.clone(),
+//             }]
+//             .into_iter()
+//             .collect::<Vec<_>>(),
+//         },
+//         o => [o].into_iter().collect::<Vec<_>>(),
+//     }
+// }
+// pub fn load_coalescing<'a, Annot: Clone>(
+//     a: impl Iterator<Item = MachOperator<'a, Annot>>,
+// ) -> impl Iterator<Item = MachOperator<'a, Annot>> {
+//     return a.scan_mach(load_coalescing_pass, ()).flatten();
+// }
